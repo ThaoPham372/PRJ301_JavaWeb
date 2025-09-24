663e6edc9a9b2daa4562fb18c657a2524b1391fb
ThaoPham372 <tuongthao02101992@gmail.com>
2025-09-24 15:32:00 +0700

    Lab 3

---
+++ SP25_Demo_MainController_1/src/java/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package service;
+
+import model.User;
+import userDAO.UserDao;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserServiceImpl implements IUserService {
+    private UserDao userDao;
+
+    public UserServiceImpl() {
+        this.userDao = new UserDao();
+    }
+
+    @Override
+    public void insertUser(User user) throws SQLException {
+        userDao.insertUser(user);
+    }
+
+    @Override
+    public User selectUser(int id) throws SQLException {
+        return userDao.selectUser(id);
+    }
+
+    @Override
+    public List<User> selectAllUsers() throws SQLException {
+        return userDao.selectAllUsers();
+    }
+
+    @Override
+    public boolean updateUser(User user) throws SQLException {
+        return userDao.updateUser(user);
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        return userDao.deleteUser(id);
+    }
+
+    @Override
+    public List<User> searchUsers(String keyword) throws SQLException {
+        return userDao.searchUsers(keyword);
+    }
+}
